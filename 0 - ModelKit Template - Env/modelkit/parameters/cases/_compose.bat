6cdfe7d115653c092994a73f6891c7ae24919902
avanigoyal-trc <56365171+avanigoyal-trc@users.noreply.github.com>
2020-01-21 15:08:55 -0800

    New 'baseline' analysis runs; hr-baseline runs

--- 0 - ModelKit Template - Env/modelkit/parameters/cases/_compose.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-REM To use, drag and drop .pxv parameter files onto this batch file.
-%~d1
-CD %~dp0
-CALL modelkit template-compose --files=%1 --output=ConsAssm-test.cibd19 ConsAssm-test.pxt
-PAUSE
