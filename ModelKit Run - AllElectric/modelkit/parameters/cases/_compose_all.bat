9ff2627441f57b1c0382ace5d9a753e465084c1f
mnakaj <MNakajima@TRCcompanies.com>
2020-01-24 15:58:21 -0800

    Add DOAS and Res & Laundry/Corridor HVAC system sizing

--- ModelKit Run - AllElectric/modelkit/parameters/cases/_compose_all.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-FOR %%f IN (*.pxv) DO CALL modelkit template-compose --files="%%f" --output="../../runs/%%~nf/instance.cibd19" ../../templates/root.pxt
-FOR /D %%d IN (..\..\runs\*.*) DO COPY /Y ..\..\bin\_run.bat "%%d"
-PAUSE
