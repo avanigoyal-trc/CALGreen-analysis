79dd9b8b3f3912c62210f5c61bf35b8b0adc78d6
mnakaj <MNakajima@TRCcompanies.com>
2020-03-09 13:10:54 -0700

    VRF pre-autosizing Results

---
+++ copy_files.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+copy ./framework/analysis/baseline-hr/runs/hr-baseline_fixeddual/cz01/instance.cibd22 "C:\Users\mnakajima\TRC\MF Title 24 Case 2022 - MF prototypes\Models\High-rise-MF\cz01.cibd22"
+
+
+PAUSE
