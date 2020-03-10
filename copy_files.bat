63fc8b8b5dbecb1dc174d4410dc22421b018d1d4
mnakaj <MNakajima@TRCcompanies.com>
2020-03-09 18:18:24 -0700

    Updated VRF Autosized MUMR results

--- copy_files.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-copy ./framework/analysis/baseline-hr/runs/hr-baseline_fixeddual/cz01/instance.cibd22 "C:\Users\mnakajima\TRC\MF Title 24 Case 2022 - MF prototypes\Models\High-rise-MF\cz01.cibd22"
-
-
-PAUSE
