a9466a44395a237fb64d61a9bea499305af2226b
mnakaj <MNakajima@TRCcompanies.com>
2020-04-15 12:00:19 -0700

    Delete unnecessary files

--- ModelKit Run - HPTE/modelkit/bin/_compose_all.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-FOR %%f IN (..\parameters\cases\*.pxv) DO CALL modelkit template-compose --files="%%f" --output="../runs/%%~nf/instance.cibd19" ../templates/root.pxt
-FOR /D %%d IN (..\runs\*.*) DO COPY /Y _run.bat "%%d"
-PAUSE
--- ModelKit Run - HPTE/modelkit/bin/_copy_all_autosizing.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-FOR /D /r %%G in (..\runs\*.*) DO copy ..\runs\%%~nxG\"instance - run"\"instance - ab - HVACSecondary.csv" ..\parameters\cases\autosizing\%%~nxG.csv
-PAUSE
