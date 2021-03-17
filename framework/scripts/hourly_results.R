6768b7c418280f3bf2fbefb86952b78a1ab5c9ad
Mia Nakajima <MNakajima@TRCcompanies.com>
2021-03-16 18:04:15 -0700

    Add Hourly Results for Low Rise

--- framework/scripts/hourly_results.R
+++ framework/scripts/hourly_results.R
@@ -4,6 +4,111 @@ library(openxlsx)
 library(data.table)
 library(readxl)
 
+hourly_results_spreadsheet_com <- function(path, measures = NULL, cz = NULL, folder = NA, file = NA, remove_DHW = FALSE){
+  ## For CBECC-Com
+  
+  ##path: String format, path to prototype results folder starting at framework ie. "analysis/hvac_autosizing"
+  if(is.null(measures)){
+    measures <- list.files(here::here(path, "runs"))
+  }
+  
+  if(is.null(cz)){
+    cz <- c("cz01", "cz02", "cz03", "cz04", "cz05", "cz06", "cz07",
+            "cz08", "cz09", "cz10", "cz11", "cz12", "cz13", "cz14",
+            "cz15", "cz16")
+  }
+  
+  
+  wb <- createWorkbook()
+  
+  for (measure in measures){
+    
+    
+    
+    cz_missing <- c()
+    
+    for(clim in cz){
+      
+      if(is.na(folder)){
+        folder <- "instance - run"
+        file <- "instance - ap - HourlyResults.csv"
+      }
+      
+      if(!(paste(clim, folder, file, sep = "/") %in% list.files(here::here(path, "runs", measure), recursive = T))){
+        cz_missing <- c(cz_missing, clim)
+        next
+      }
+      
+      hourly <- read_csv(here::here(path, "runs", measure, clim, folder, file), skip = 16)
+      
+      cfa_df <- read_csv(here::here(path, "runs", measure, clim, folder, file), skip = 10, col_names = FALSE)
+      
+      cfa <- as.numeric(cfa_df$X4[1])
+      
+      clim_num <- as.numeric(str_extract(clim, "\\d+"))
+      
+      hourly_totals_results <- hourly %>% select(Mo, Da, Hr, `(kWh)_5`, `(kWh)_14`, `(kBtu)_5`, `(kBtu)_12`, `(kWh)_13`, `(kBtu)_11`)
+      
+     
+      
+      
+      
+      names(hourly_totals_results) <- c("Month", "Day", "Hour", "DHW_kWh", "Elec_kWh", "DHW_kBtu", "NG_kBtu", "Elec_Comp_kWh", "NG_Comp_kBtu")
+      
+      if(remove_DHW){
+        
+        hourly_totals_results <- hourly_totals_results %>%
+          mutate(Elec_kWh = Elec_kWh - DHW_kWh) %>%
+          mutate(NG_kBtu = NG_kBtu - DHW_kBtu) %>%
+          mutate(Elec_Comp_kWh = Elec_Comp_kWh - DHW_kWh) %>%
+          mutate(NG_Comp_kBtu = NG_Comp_kBtu - DHW_kBtu) %>%
+          select(-DHW_kWh, -DHW_kBtu)
+        
+      }
+      
+      
+      hourly_results <- hourly_totals_results %>% rowwise() %>% 
+        mutate(CZ = as.numeric(str_extract_all(clim, "\\d+"))) %>%
+        select(Month, Day, Hour, CZ, Elec_kWh, NG_kBtu, Elec_Comp_kWh, NG_Comp_kBtu)  
+      
+      
+      
+      
+      assign( clim, hourly_results, envir = .GlobalEnv)
+      
+    }
+    
+    cz_list <- cz[!(cz %in% cz_missing)]
+    
+    all_results <- data.table::rbindlist(lapply(cz_list, get)) %>% mutate(Measure = measure)
+    
+    
+    
+    assign(measure, all_results, envir = .GlobalEnv) 
+    
+    if(!(measure %in% names(wb))){
+      
+      addWorksheet(wb, measure)
+      
+      
+    }
+    
+    writeData(wb, measure, all_results)
+    
+    
+  }
+  
+  
+  
+  
+  addWorksheet(wb, "Description")
+  description_of_run <- paste( "DHW Removed: ", remove_DHW, "Date Produced:", Sys.Date(),  sep = " ")
+  writeData(wb, "Description", description_of_run)
+  
+  saveWorkbook(wb, here::here(path, "HourlyResults.xlsx"), overwrite = TRUE)
+  
+}
+
 tdv_2022_generate_com <- function(path, measures = NULL, cz = NULL, lifetime, folder = NA, file = NA, res_or_nr = "nr", remove_DHW = FALSE){
   ## For CBECC-Com
   
@@ -142,6 +247,122 @@ tdv_2022_generate_com <- function(path, measures = NULL, cz = NULL, lifetime, fo
   saveWorkbook(wb, here::here(path, "TDVAnnual.xlsx"), overwrite = TRUE)
   
 }
+
+hourly_results_spreadsheet_res <- function(path, measures = NULL, cz = NULL,  file = NA,  remove_DHW = FALSE, remove_PVB = FALSE){
+  ## For CBECC-Res
+  
+  ##path: String format, path to prototype results folder starting at framework ie. "analysis/hvac_autosizing"
+  if(is.null(measures)){
+    measures <- list.files(here::here(path, "runs"))
+  }
+  
+  if(is.null(cz)){
+    cz <- c("cz01", "cz02", "cz03", "cz04", "cz05", "cz06", "cz07",
+            "cz08", "cz09", "cz10", "cz11", "cz12", "cz13", "cz14",
+            "cz15", "cz16")
+  }
+  
+  
+  
+  wb <- createWorkbook()
+  
+  for (measure in measures){
+    
+    
+    
+    cz_missing <- c()
+    
+    for(clim in cz){
+      
+      if(is.na(file)){
+        file <- "instance - Prop - HourlyResults.csv"
+      }
+      
+      if(!(paste(clim, file, sep = "/") %in% list.files(here::here(path, "runs", measure), recursive = T))){
+        cz_missing <- c(cz_missing, clim)
+        next
+      }
+      
+      hourly <- read_csv(here::here(path, "runs", measure, clim,  file), skip = 14)
+      
+      cfa_df <- read_csv(here::here(path, "runs", measure, clim,  file), skip = 7, col_names = FALSE)
+      
+      cfa <- as.numeric(cfa_df$X4[1])
+      
+      clim_num <- as.numeric(str_extract(clim, "\\d+"))
+      
+      hourly_totals <- hourly %>%
+        select(Mo, Da, Hr, `(kWh)`,`(kWh)_1`,`(kWh)_2`,`(kWh)_3`,`(kWh)_4`,`(kWh)_5`,`(kWh)_10`,`(kWh)_11`, `(kWh)_12`, `(Therms)`,`(Therms)_1`,`(Therms)_2`,`(Therms)_3`,`(Therms)_4`,`(Therms)_5`, `(Therms)_10`, `(TDV/Btu)`, `(TDV/Btu)_1`) %>%
+        rowwise() %>%
+        mutate(Elec_kWh_Comp = sum(`(kWh)`,`(kWh)_1`,`(kWh)_2`,`(kWh)_3`,`(kWh)_4`,`(kWh)_5`)) %>%
+        mutate(NG_Therm_Comp = sum(`(Therms)`,`(Therms)_1`,`(Therms)_2`,`(Therms)_3`,`(Therms)_4`,`(Therms)_5`)) %>%
+        mutate(Elec_kWh_PVB = sum(`(kWh)_10`,`(kWh)_11`)) %>%
+        select(Mo, Da, Hr,`(kWh)_4`, Elec_kWh_PVB, `(kWh)_12`, Elec_kWh_Comp, `(Therms)_4`, `(Therms)_10`,NG_Therm_Comp)
+      
+      
+      
+      names(hourly_totals) <- c("Month", "Day", "Hour", "DHW_kWh", "Elec_kWh_PVB", "Elec_kWh", "Elec_kWh_Comp", "DHW_Therm", "NG_Therm", "NG_Therm_Comp")
+      
+      if(remove_DHW){
+        
+        hourly_totals <- hourly_totals %>%
+          mutate(Elec_kWh = Elec_kWh - DHW_kWh) %>%
+          mutate(NG_Therm = NG_Therm - DHW_Therm) %>%
+          mutate(Elec_kWh_Comp = Elec_kWh_Comp - DHW_kWh) %>%
+          mutate(NG_Therm_Comp = NG_Therm_Comp - DHW_Therm) %>%
+          select(-DHW_kWh, -DHW_Therm)
+        
+      }
+      
+      if(remove_PVB){
+        
+        hourly_totals <- hourly_totals %>%
+          mutate(Elec_kWh = Elec_kWh - Elec_kWh_PVB) %>%
+          select(-Elec_kWh_PVB)
+        
+        
+      }
+      
+      
+      hourly_totals_with_tdv <- hourly_totals %>% rowwise() %>% 
+        mutate(CZ = as.numeric(str_extract_all(clim, "\\d+"))) %>%
+        select(Month, Day, Hour, CZ, Elec_kWh, NG_Therm, Elec_kWh_Comp, NG_Therm_Comp)
+      
+      
+      
+      assign( clim, hourly_totals_with_tdv, envir = .GlobalEnv)
+      
+    }
+    
+    cz_list <- cz[!(cz %in% cz_missing)]
+    
+    all_results <- data.table::rbindlist(lapply(cz_list, get)) %>% mutate(Measure = measure)
+    
+    
+    
+    assign(measure, all_results, envir = .GlobalEnv) 
+    
+    if(!(measure %in% names(wb))){
+      
+      addWorksheet(wb, measure)
+      
+      
+    }
+    
+    writeData(wb, measure, all_results) 
+    
+  }
+  
+  
+  
+  addWorksheet(wb, "Description")
+  description_of_run <- paste("DHW Removed: ", remove_DHW, "; PVB Removed: ", remove_PVB, "; Date Produced: ", Sys.Date(), sep = " ")
+  writeData(wb, "Description", description_of_run)
+  
+  saveWorkbook(wb, here::here(path, "HourlyResults.xlsx"), overwrite = TRUE)
+  
+}
+
 
 tdv_2022_generate_res <- function(path, measures = NULL, cz = NULL, lifetime,  file = NA, res_or_nr = "nr", remove_DHW = FALSE, remove_PVB = FALSE){
   ## For CBECC-Res
